db97d748926ddde69b7820eaf8a9385779b77013
MaximBalaganskiy <m.balaganskiy@gmail.com>
2013-02-27 23:25:29 +1100

    Added Puzzle2 code with relevant tests.

---
+++ PuzzlesLibrary/TriangleServices.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PuzzlesLibrary
+{
+    /// <summary>
+    /// Represents the static class holding triangle service functions.
+    /// </summary>
+    public static class TriangleServices
+    {
+
+        #region Subtypes
+
+        /// <summary>
+        /// Defines possible triangle types.
+        /// </summary>
+        public enum TriangleType
+        {
+            /// <summary>
+            /// Three equal sides.
+            /// </summary>
+            Equilateral = 3,
+            /// <summary>
+            /// Two equal sides.
+            /// </summary>
+            Isosceles = 1,
+            /// <summary>
+            /// No equal sides.
+            /// </summary>
+            Scalene = 0,
+            /// <summary>
+            /// Invalid triangle.
+            /// </summary>
+            Error = 4
+        };
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Returns the type of the triangle with specified side lengths.
+        /// </summary>
+        /// <param name="a">The length of the side A.</param>
+        /// <param name="b">The length of the side B.</param>
+        /// <param name="c">The length of the side C.</param>
+        /// <returns>The type of the triangle.</returns>
+        public static TriangleType GetTriangleType(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException("Invalid side length.");
+            }
+
+            // the sum of any 2 side lenghts of a valid triangle should be greater than the length of the remaining side
+            if (a + b < c || a + c < b || b + c < a)
+            {
+                return TriangleType.Error;
+            }
+
+            // the number of equalities identifies the type of the triangle
+            int equalities = (a == b ? 1 : 0) + (a == c ? 1 : 0) + (b == c ? 1 : 0);
+            return (TriangleType)equalities;
+        }
+
+        #endregion
+
+    }
+}
---
+++ PuzzlesTest/Puzzle2UnitTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzlesLibrary;
+
+namespace PuzzlesTest
+{
+    /// <summary>
+    /// Represents the Puzzle2 unit test.
+    /// </summary>
+    [TestClass]
+    public class Puzzle2UnitTest
+    {
+        /// <summary>
+        /// Tests if passing negative lenghts raises an exception.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NegativeSideGetTriangleTypeTest()
+        {
+            var type = TriangleServices.GetTriangleType(-1, 1, 2);
+        }
+
+        /// <summary>
+        /// Tests if the GetTriangleType function is able to identify an invalid triangle.
+        /// </summary>
+        [TestMethod]
+        public void InvalidTriangleGetTriangleTypeTest()
+        {
+            Assert.AreEqual(TriangleServices.TriangleType.Error, TriangleServices.GetTriangleType(1, 2, 4));
+        }
+
+        /// <summary>
+        /// Tests if the GetTriangleType function is able to identify a scalene triangle.
+        /// </summary>
+        [TestMethod]
+        public void ScaleneGetTriangleTypeTest()
+        {
+            Assert.AreEqual(TriangleServices.TriangleType.Scalene, TriangleServices.GetTriangleType(3, 4, 5));
+        }
+
+        /// <summary>
+        /// Tests if the GetTriangleType function is able to identify an equilateral triangle.
+        /// </summary>
+        [TestMethod]
+        public void EquilateralGetTriangleTypeTest()
+        {
+            Assert.AreEqual(TriangleServices.TriangleType.Equilateral, TriangleServices.GetTriangleType(3, 3, 3));
+        }
+
+        /// <summary>
+        /// Tests if the GetTriangleType function is able to identify an isosceles triangle.
+        /// </summary>
+        [TestMethod]
+        public void IsoscelesGetTriangleTypeTest()
+        {
+            Assert.AreEqual(TriangleServices.TriangleType.Isosceles, TriangleServices.GetTriangleType(3, 3, 4));
+        }
+
+    }
+}
