a09be24375599b42dbbaf184ad53c81cb40354ea
MaximBalaganskiy <m.balaganskiy@gmail.com>
2013-02-27 16:48:29 +1100

    Puzzle3 and related tests.

---
+++ PuzzlesLibrary/StringExtensions.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PuzzlesLibrary
+{
+    /// <summary>
+    /// Represents the static class providing extension methods to the string class.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Stores the initial size of the result array. The array gets incremented with this number if the number of words is greater than current array capacity.
+        /// </summary>
+        private readonly static int wordsInitialSize = 10;
+
+        /// <summary>
+        /// Stores the initial size of the word array. The array gets incremented with this number if the number of characters is greater than current array capacity.
+        /// </summary>
+        private readonly static int wordInitialSize = 10;
+
+        /// <summary>
+        /// Splits the given string using the specified separator.
+        /// </summary>
+        /// <param name="str">The string to be split.</param>
+        /// <param name="separator">The separator.</param>
+        /// <returns>The array of words contained in the spesified string.</returns>
+        public static string[] PuzzleSplit(this string str, char separator)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException("str", "The method is called on a null string.");
+            }
+
+            if (str.Length == 0)
+            {
+                return new string[1] { string.Empty };
+            }
+
+            // the array of words with initial size of wordsCacheSize which will be increased should the need arise
+            string[] result = new string[wordsInitialSize];
+            int words = 0;
+            // the array of chars building the current word. The initial size will be increased should the need arise
+            char[] word = new char[wordInitialSize];
+            int wordLength = 0;
+            // the loop is deliberately made to reach str.Length to allow for flushing the last word to the array
+            for (int i = 0; i <= str.Length; i++)
+            {
+                // put the character to the word being built if it is not a separator
+                if (i < str.Length && str[i] != separator)
+                {
+                    // increase the size of the word array if current word does not fit
+                    if (wordLength == word.Length)
+                    {
+                        // depending on the typical word length this could be changed to implement more memory expensive (e.g. exponential) resize strategy
+                        Array.Resize(ref word, word.Length + wordInitialSize);
+                    }
+
+                    word[wordLength++] = str[i];
+                }
+
+                // flush the word being built to the result array if separator or the character "after the last" occurs
+                if (i == str.Length || str[i] == separator)
+                {
+                    // increase the size of the result array if there is not enough space for a new word
+                    if (words == result.Length)
+                    {
+                        // depending on the typical number of words this could be changed to implement more memory expensive (e.g. exponential) resize strategy
+                        Array.Resize(ref result, result.Length + wordsInitialSize);
+                    }
+
+                    result[words++] = new string(word, 0, wordLength);
+                    // clear the word being built
+                    wordLength = 0;
+                }
+
+            }
+
+            // trim the result array to the actual size
+            Array.Resize(ref result, words);
+            return result;
+        }
+
+        /// <summary>
+        /// Reverses the characters in the specified string and returns the result as a new string.
+        /// </summary>
+        /// <param name="str">The string to reverse.</param>
+        /// <returns>The reversed string.</returns>
+        public static string PuzzleReverse(this string str)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException("str", "The method is called on a null string.");
+            }
+
+            if (str.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            char[] result = new char[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                result[str.Length - 1 - i] = str[i];
+            }
+
+            return new string(result);
+        }
+
+        /// <summary>
+        /// Reverses every word in the specified string.
+        /// </summary>
+        /// <param name="str">The string to reverse words in.</param>
+        /// <param name="separator">The words separator.</param>
+        /// <returns>The string with reversed words in it.</returns>
+        public static string ReverseWords(this string str, char separator)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException("str", "The method is called on a null string.");
+            }
+
+            if (str.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            var sb = new StringBuilder();
+            // split the string into words
+            string[] words = str.PuzzleSplit(separator);
+            for (int i = 0; i < words.Length; i++)
+            {
+                // reverse each word and append it to the result
+                sb.Append(words[i].PuzzleReverse());
+                // append a seperator if the word is not the last
+                if (i < words.Length - 1)
+                {
+                    sb.Append(separator);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ PuzzlesTest/Puzzle3UnitTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzlesLibrary;
+
+namespace PuzzlesTest
+{
+    [TestClass]
+    public class Puzzle3UnitTest
+    {
+        [TestMethod]
+        public void SplitTest()
+        {
+            string str = "  one reeeeeeeeeeeealy long word is not enough so,   i will add more than 10 words to see the result   ";
+            string[] res = str.PuzzleSplit(' ');
+            string[] expected = str.Split(' ');
+            CollectionAssert.AreEqual(expected, res);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullStringSplitTest()
+        {
+            string str = null;
+            string[] res = str.PuzzleSplit(' ');
+        }
+
+        [TestMethod]
+        public void EmptyStringSplitTest()
+        {
+            string str = string.Empty;
+            string[] res = str.PuzzleSplit(' ');
+            string[] expected = str.Split(' ');
+            CollectionAssert.AreEqual(expected, res);
+        }
+
+        [TestMethod]
+        public void ReverseTest()
+        {
+            Assert.AreEqual("eno", "one".PuzzleReverse());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullStringReverseTest()
+        {
+            string str = null;
+            str.PuzzleReverse();
+        }
+
+        [TestMethod]
+        public void EmptyStringReverseTest()
+        {
+            Assert.AreEqual(string.Empty, string.Empty.PuzzleReverse());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullStringReverseWordsTest()
+        {
+            string str = null;
+            str.ReverseWords(' ');
+        }
+
+        [TestMethod]
+        public void ReverseWordsTest()
+        {
+            Assert.AreEqual("  tac dna    god  ", "  cat and    dog  ".ReverseWords(' '));
+        }
+
+        [TestMethod]
+        public void EmptyStringReverseWordsTest()
+        {
+            Assert.AreEqual(string.Empty, string.Empty.ReverseWords(' '));
+        }
+
+    }
+}
